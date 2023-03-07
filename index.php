d16c675f9beeabb018119f644540d58ae62e5e5a
Kaoutiz <94445745+Kaoutiz@users.noreply.github.com>
2023-03-07 19:22:28 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+
+    <!-- BEGIN: LIBRAIRIES -->
+    <link rel="stylesheet" href="assets/css/boxicons.css">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- END: LIBRAIRIES -->
+
+    <!-- BEGIN: STYLES -->
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <!-- END: STYLES -->
+</head>
+    <body>
+        <!-- BEGIN: JQuery -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <!-- END: JQuery -->
+        <header>
+            <div class="container">
+                <nav>
+                    <ul class="nav-left">
+                        <li><a href="#">Qui sommes-npus ?</a></li>
+                        <li><a href="#">Catalogue</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                    <ul class="nav-right">
+                        <li><a href="#">Se connecter</a></li>
+                        <i class='bx bx-menu'></i>
+                    </ul>
+                </nav>
+                <div id="title-site">
+                    <h1><span class="title-site-light">iter</span>space</h1>
+                    <h2>Un chemin vers l'espace</h2>
+                    <button class="rounded-btn">Réservez maintenant</button>
+                </div>
+                <div id="compteur">
+                    <div class="compteur-box">
+                        <p class="number">958</p>
+                        <p class="title-compteur">Voyage effectués</p>   
+                    </div>
+                    <div class="compteur-box">
+                        <p class="number">1052</p>
+                        <p class="title-compteur">Clients satisfait</p>   
+                    </div>
+                    <div class="compteur-box">
+                        <p class="number">4</p>
+                        <p class="title-compteur">Fusées dispnibles</p>   
+                    </div>
+                </div>
+            </div>
+        </header>
+        <section id="presentation">
+            <div class="container">
+                <!-- Votre code ici... -->
+            </div>
+        </section>
+        <section id="actus">
+            <div class="container">
+                <!-- Votre code ici... -->
+            </div>
+        </section>
+        <section id="reservation">
+            <div class="container">
+                <!-- Votre code ici... -->
+            </div>
+        </section>
+        <section id="produits">
+            <div class="container">
+                <!-- Votre code ici... -->
+            </div>
+        </section>
+        <section id="testimonial">
+            <div class="container">
+                <!-- Votre code ici... -->
+            </div>
+        </section>
+        <footer>
+            <div class="container">
+                <!-- Votre code ici... -->
+            </div>
+        </footer>
+    </body>
+    <!-- BEGIN: LIBRAIRIES SCRIPTS -->
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <!-- END: LIBRAIRIES SCRIPTS -->
+
+    <!-- BEGIN: SCRIPTS -->
+    <!-- BEGIN: SCRIPTS -->
+</html>
