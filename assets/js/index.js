076830b764a7fa66e67c7121c591427481e5235c
Kaoutiz <94445745+Kaoutiz@users.noreply.github.com>
2023-03-15 18:47:56 +0100

    effect number

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,11 @@
 
 import { Scroll } from "./scroll.js";
+import { numberEffect } from "./numberEffect.js";
 
 document.addEventListener("DOMContentLoaded", function(event) {
     
     Scroll();
+    numberEffect();
 
 });
   
