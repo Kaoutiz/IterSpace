07cb46db2632e80db670f32f3a8dfec533370461
Eld0d0 <nils.gill.arion@gmail.com>
2023-03-19 14:51:50 +0100

    Page de présentation

--- pages/qui-sommes-nous.php
+++ pages/qui-sommes-nous.php
@@ -11,23 +11,80 @@
     <link rel="stylesheet" href="/iterspace/assets/css/boxicons.css">
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <!-- END: LIBRAIRIES -->
 
     <!-- BEGIN: STYLES -->
     <link rel="stylesheet" href="/iterspace/assets/css/range.css">
     <link rel="stylesheet" href="/iterspace/assets/css/styles.css">
+    <link rel="stylesheet" href="/iterspace/assets/css/qui-sommes-nous.css">
     <!-- END: STYLES -->
 </head>
 
 <body>
     <?php include '../assets/templates/general_header.php'; ?>
 
+    <section id="presentation">
+        <div class="container">
+            <div class="section-title">
+                <h3 class="title">Presentation</h3>
+            </div>
+            <div class="row">
+                <div id="presentation-p" class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6" data-aos="fade-down">
+                    <h4>Nos valeurs</h4>
+                    <p>
+                        Bienvenue dans le monde passionnant d'Iterspace, une société pionnière dans le domaine du
+                        tourisme spatial. Nous sommes fiers de vous présenter notre offre exclusive de voyages
+                        touristiques dans l'espace, qui vous emmèneront au-delà de l'atmosphère terrestre pour
+                        vivre une expérience unique et inoubliable.
+                    </p>
+                    <p>
+                        Notre entreprise est fondée sur une passion
+                        pour l'exploration spatiale et la découverte de nouveaux horizons. Nous sommes convaincus
+                        que l'avenir de l'humanité se trouve dans l'exploration de l'espace, et nous avons donc
+                        créé Iterspace pour permettre à chacun de réaliser ce rêve.
+                    </p>
+                </div>
+                <div id="presnation-img" class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 text-center" data-aos="fade-left">
+                    <img src="/iterspace/assets/img/spaceship.png" class="spaceship" alt="vaisseau-spatial" />
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="voyage">
+        <div class="container">
+            <div class="row">
+                <div id="presnation-img" class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 text-center" data-aos="fade-left">
+                    <img src="/iterspace/assets/img/iss_schema.png" class="iss_schema" alt="iss_schema" />
+                </div>
+                <div id="presentation-p" class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6" data-aos="fade-down">
+                    <h4>Nos voyages</h4>
+                    <p>
+                        Nos voyages sont conçus pour répondre aux besoins de tous les types de voyageurs,
+                        des aventuriers intrépides aux familles curieuses. Nous proposons des itinéraires
+                        variés, allant de courts vols suborbitaux à des séjours prolongés dans des stations
+                        orbitales.
+                    </p>
+                    <p>
+                        Nos navettes spatiales sont équipées des dernières technologies pour garantir la
+                        sécurité et le confort de nos clients. Nous travaillons avec les meilleurs professionnels
+                        du secteur pour vous offrir une expérience de voyage inoubliable.
+                    </p>
+                    <p>
+                        Que vous soyez passionné d'astronomie, de science-fiction ou simplement en quête d'une
+                        aventure unique, Iterspace est la société idéale pour vous. Nous sommes impatients de
+                        vous accueillir à bord de nos navettes spatiales et de vous faire découvrir les merveilles
+                        de l'espace.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <?php include '../assets/templates/footer.php'; ?>
 </body>
 
 <!-- BEGIN: LIBRAIRIES SCRIPTS -->
-<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 <!-- END: LIBRAIRIES SCRIPTS -->
